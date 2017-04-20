18f00227b668b66e663755ff93d32319c1753e09
int-0 <tobias.deb@gmail.com>
2017-04-20 19:39:31 +0200

    Initial packaging script

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+## Visit https://github.com/replica-con-k/retropi3d_engine" ##
+## for licensing info                                       ##
+#
+
+from setuptools import setup
+
+setup(name='replika',
+      version='0.1',
+      description='Library for make retro games easily',
+      url='https://github.com/replica-con-k/retropi3d_engine',
+      author='Int-0',
+      author_email='tobias.deb@gmail.com',
+      license='GPL v3.0',
+      packages=['replika'],
+      package_dir={'replika': 'src'},
+      zip_safe=False)
+
